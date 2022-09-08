137923035be171a23deaef1fc947ccea85638aa2
ahhaahaa <99873728+ahhaahaa@users.noreply.github.com>
2022-09-08 13:13:15 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function SendMessage() { 
+ const inputName = document.getElementById("name"); 
+ const inputEmail = document.getElementById("email"); 
+ const inputTel = document.getElementById("email"); 
+ if (inputName.value == "" && inputEmail.value == "" && inputTel.value == "" &&) { 
+ inputName.style.borderColor = "red"; 
+ inputEmail.style.borderColor = "red"; 
+ input.style.borderColor = "red"; 
+ } 
+ }
